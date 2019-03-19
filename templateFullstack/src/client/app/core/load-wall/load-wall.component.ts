85eb1f0038efb9ce51de1ff83ccd0c546d5d9a3c
NickSym <nickamorg0@gmail.com>
2019-03-19 13:29:32 +0200

    Added New Wall Page/Overlay and a basic Load Wall Page/Overlay -- Both without functionality

--- templateFullstack/src/client/app/core/load-wall/load-wall.component.ts
+++ templateFullstack/src/client/app/core/load-wall/load-wall.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-load-wall',
   templateUrl: './load-wall.component.html',
   styleUrls: ['./load-wall.component.scss'],
 })
-export class LoadWallComponent { }
+
+export class LoadWallComponent { 
+  
+}
--- templateFullstack/src/client/app/core/new-wall/new-wall.component.ts
+++ templateFullstack/src/client/app/core/new-wall/new-wall.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-new-wall',
   templateUrl: './new-wall.component.html',
   styleUrls: ['./new-wall.component.scss'],
 })
