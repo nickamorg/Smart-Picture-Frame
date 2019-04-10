097e95fd6b1225d2aff7233cd509655ad947de55
NickSym <nickamorg0@gmail.com>
2019-04-10 16:08:25 +0300

    Gallery: Dynamic image loading with overlay for options and details display

--- templateFullstack/src/client/app/core/gallery/gallery.component.ts
+++ templateFullstack/src/client/app/core/gallery/gallery.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.scss']
 })
+
+
 export class GalleryComponent implements OnInit {
+    imagesCol: number;
+    galleryImages: Image[];
 
-  constructor() { }
+    constructor() { 
+        this.imagesCol = 6;
 
-  ngOnInit() {
-  }
+        this.galleryImages = [
+            new Image("Amazon Waterfall", "Description Example", "waterfall1.png"),
+            new Image("Amazon Waterfall", "Description Example", "waterfall2.png"),
+            new Image("Amazon Waterfall", "Description Example", "waterfall3.png"),
+            new Image("Amazon Waterfall", "Description Example", "waterfall4.png")
+        ]
+    }
 
+    ngOnInit() { }
+
+    changeImagesCol(col:number) {
+        this.imagesCol = col;
+    }
+}
+
+class Image {
+    title: string;
+    description: string;
+    src: string;
+
+    constructor(title, descr, src) {
+        this.title = title;
+        this.description = descr;
+        this.src = src;
+    }
 }
