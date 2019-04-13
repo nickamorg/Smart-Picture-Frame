a7a91d2a222de6a238fdcb176ae53e76a4168616
NickSym <nickamorg0@gmail.com>
2019-04-13 16:23:19 +0300

    Wall display -- Static example of a very simple wall

--- templateFullstack/src/client/app/core/upload-images/upload-images.component.ts
+++ templateFullstack/src/client/app/core/upload-images/upload-images.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-upload-images',
-  templateUrl: './upload-images.component.html',
-  styleUrls: ['./upload-images.component.scss']
+    selector: 'app-upload-images',
+    templateUrl: './upload-images.component.html',
+    styleUrls: ['./upload-images.component.scss']
 })
 export class UploadImagesComponent implements OnInit {
+    walls: {};
 
-  constructor() { }
+    constructor() { 
+        this.walls = {
+            "wall1": {
+                "title": "example glossary",
+                "creator": "Home",
+                "type": "General",
+                "target": "Family",
+                "description": "Just a usual wall",
+                "border-size": 5,
+                "border-material": "gold.png",
+                "display-time": "20: 30",
+                "duration": 30,
+                "repeat-time": "02:30:00",
+                "wallpapers-iterate-time": 30,
+                "wallpapers": {
+                    "image1": "waterfall.png",
+                    "image2": "waterfall2.png"
+                },
+                "color": "#ffffff",
+                "frames": {
+                    "frame1": {
+                    "shape": "rectangle",
+                    "border-size": 5,
+                    "border-material": "gold.png",
+                    "padding": 10,
+                    "top": 30,
+                    "left": 50,
+                    "width": 100,
+                    "height": 100,
+                    "images": {
+                        "image1": "waterfall.png",
+                        "image2": "waterfall2.png"
+                    },
+                    "iterate-time": 30
+                    }
+                }
+            }
+        };
 
-  ngOnInit() {
-  }
+        console.log(this.walls);
+    }
+
+    ngOnInit() { }
 
 }
