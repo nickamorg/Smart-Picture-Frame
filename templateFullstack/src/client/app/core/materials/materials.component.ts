25e8e1eaa4c2e96bb20c531f62687f6902c22ab4
NickSym <nickamorg0@gmail.com>
2019-05-13 17:17:25 +0300

    Materials Page: Upload and display new materials -- No stored in db and without use elsewhere

--- templateFullstack/src/client/app/core/materials/materials.component.ts
+++ templateFullstack/src/client/app/core/materials/materials.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class MaterialsComponent implements OnInit {
     materials: Material[];
+    uploadedMaterials: Material[];
+    imagesToBeUploaded: boolean = false;
 
     constructor() { 
         this.materials = [
@@ -24,6 +26,56 @@ export class MaterialsComponent implements OnInit {
 
     deleteMaterial(src: string) {
         this.materials = this.materials.filter(function(elem){
+            return elem.src != src;
+        });
+    }
+
+    processFile(imageInput) {
+        console.log(imageInput);
+        this.uploadedMaterials = [];
+        this.imagesToBeUploaded = true;
+        for(var i = 0; i < imageInput.files.length; i++) {
+         
+            var file: File = imageInput.files[i];
+            var reader = new FileReader();
+            reader.addEventListener('load', (event: any) => {
+                //console.log(event.target.result, file)
+                this.uploadedMaterials.push(new Material(event.target.result, ""));
+                console.log(this.uploadedMaterials.length);
+                /*
+                this.selectedFile = new ImageSnippet(event.target.result, file);
+            
+                this.imageService.uploadImage(this.selectedFile.file).subscribe(
+                    (res) => {
+                    
+                    },
+                    (err) => {
+                    
+                })
+
+                */
+            });
+        
+            reader.readAsDataURL(file);
+        }
+    }
+
+    uploadMaterials() {
+        //Mongodb code missed
+        this.imagesToBeUploaded = false;
+        console.log(this.uploadedMaterials);
+    }
+
+    cancelUploadMaterials() {
+        this.imagesToBeUploaded = false;
+    }
+
+    saveMaterialTitle(index, event) {
+        this.uploadedMaterials[index].title = event.target.value;
+    }
+
+    deleteUploadedMaterial(src: string) {
+        this.uploadedMaterials = this.uploadedMaterials.filter(function(elem){
             return elem.src != src;
         });
     }
