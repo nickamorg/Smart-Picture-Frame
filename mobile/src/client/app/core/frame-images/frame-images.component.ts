4fb329ac349f1c3ceb242f3ace4248ef958631be
NickSym <nickamorg0@gmail.com>
2019-05-29 19:50:37 +0300

    Mobile: Tool Page -- Frame/Wall images selection

--- mobile/src/client/app/core/frame-images/frame-images.component.ts
+++ mobile/src/client/app/core/frame-images/frame-images.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ShapesService } from '../../shapes.service';
+import { DatabaseService } from '../../database.service';
+import { GalleryImage } from '../../galleryImage';
+import { GalleryComponent } from '../gallery/gallery.component';
 
 @Component({
     selector: 'app-frame-images',
@@ -11,18 +14,39 @@ export class FrameImagesComponent implements OnInit {
     @Output() messageEvent = new EventEmitter<string>();
 
     showChooseFrameImages = false;
-    constructor(private shapesService: ShapesService) {}
     showFiltersModal = false;
+    images: GalleryImage[] = [];
+    private galleryComponent: GalleryComponent;
+
+    constructor(private shapesService: ShapesService, private databaseService: DatabaseService) {
+        this.getImages();
+        this.galleryComponent = new GalleryComponent(databaseService);
+    }
+
+    getImages() {
+        this.databaseService.getImages().subscribe(
+            images => {
+                this.images = images;
+            }
+        );
+    }
 
     ngOnInit() { }
 
     sendMessage() {
         this.messageEvent.emit('toggleChooseFrameImagesModal');
+        this.shapesService.currFrameImages = [];
+        this.shapesService.selectedImages = this.shapesService.frameImages.length;
+        this.shapesService.frameImages.forEach(image => {
+            this.shapesService.currFrameImages.push(image);
+        });
     }
 
     applySelectedImages() {
-        this.shapesService.addSelectedImages();
-        this.sendMessage();
+        if (this.shapesService.selectedImages) {
+            this.shapesService.addSelectedImages();
+            this.sendMessage();
+        }
     }
 
     openFiltersModal() {
@@ -36,4 +60,5 @@ export class FrameImagesComponent implements OnInit {
     applyFilters() {
         this.showFiltersModal = false;
     }
+
 }
--- mobile/src/client/app/core/wall-images/wall-images.component.ts
+++ mobile/src/client/app/core/wall-images/wall-images.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ShapesService } from '../../shapes.service';
+import { Wallpaper } from '../../wallpaper';
+import { WallpaperDBService } from '../../wallpaperDB.service';
 
 @Component({
   selector: 'app-wall-images',
@@ -7,21 +9,41 @@ import { ShapesService } from '../../shapes.service';
   styleUrls: ['./wall-images.component.scss']
 })
 export class WallImagesComponent implements OnInit {
+
     @Input() showChooseWallImagesModalPartially: boolean;
     @Output() messageEvent = new EventEmitter<string>();
 
     showChooseWallImages = false;
-    constructor(private shapesService: ShapesService) {}
+    wallpapers: Wallpaper[] = [];
+
+    constructor(private shapesService: ShapesService, private wallpaperDBService: WallpaperDBService) {
+        this.getWallpapers();
+    }
+
+    getWallpapers() {
+        this.wallpaperDBService.getWallpapers().subscribe(
+            wallpapers => {
+                this.wallpapers = wallpapers;
+            }
+        );
+    }
 
     ngOnInit() { }
 
     sendMessage() {
         this.messageEvent.emit('toggleChooseWallImagesModal');
+        this.shapesService.currWallpapers = [];
+        this.shapesService.selectedWallpapers = this.shapesService.wallpapers.length;
+        this.shapesService.wallpapers.forEach(wallpaper => {
+            this.shapesService.currWallpapers.push(wallpaper);
+        });;
     }
 
-    applySelectedWallImages() {
-        this.shapesService.addSelectedWallpapers();
-        this.sendMessage();
+    applySelectedWallpapers() {
+        if (this.shapesService.selectedWallpapers) {
+            this.shapesService.addSelectedWallpapers();
+            this.sendMessage();
+        }
     }
 
 }
